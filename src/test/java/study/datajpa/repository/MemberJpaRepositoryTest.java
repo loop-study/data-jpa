13edb3536f5b585e4d7cb0567705f9908a56cbff
apdkeb <apdkeb@naver.com>
2021-02-27 17:18:32 +0900

    Initial commit

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,33 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void 생성테스트() throws Exception {
+        //given
+        Member member = new Member("memberA");
+
+        //when
+        Member savedMember = memberJpaRepository.save(member);
+        Member findMember = memberJpaRepository.find(member.getId());
+
+        //then
+        assertThat(savedMember.getId()).isEqualTo(findMember.getId());
+
+    }
+}
