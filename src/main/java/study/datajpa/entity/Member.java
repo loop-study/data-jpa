0aed93971dddfaf0a1fa8359ad80def83fd42702
apdkeb <apdkeb@naver.com>
2021-03-01 15:18:49 +0900

    예제 도메인 모델 추가

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,25 +1,47 @@
 package study.datajpa.entity;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"}) // team 찍으면 무한루프 발생위험
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-
     private String username;
+    private int age;
 
-    public Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+//    protected Member() {
+//    }
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    // 양방향
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
---
+++ src/main/java/study/datajpa/service/MemberService.java
@@ -0,0 +1,4 @@
+package study.datajpa.service;
+
+public class MemberService {
+}
